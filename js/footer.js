731f8b8830088da60885cf1dcbef963e31a57ac9
Yuxuan Chen <yuxuanc@umich.edu>
2023-11-07 21:42:49 -0500

    make navbar js and update site content

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    document.getElementById('insertFooter').innerHTML =`
+        <footer class="px-4 py-2">
+            <p>
+                Copyright &copy; 2023
+            </p>
+        </footer>`;
+});
---
+++ js/navbar.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    document.getElementById('insertNavBar').innerHTML = `
+    <div id="mlag-logo">
+      <a href="index.html"><img src="img/logo.png" alt="Michigan Leagues of Academic Games Logo" ></a>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mlag-navbar" aria-controls="mlag-navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mlag-navbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="tournaments.html">2023-2024 Tournaments</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="tournament_results.html">Super Tournament Results</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="games.html">Games</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="documents.html">Documents</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">MLAG Steering Committee</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+        `;
+});
