b162f365ff217bbe14220e55d46b8d4dbaa63748
Yuxuan Chen <yuxuanc@umich.edu>
2024-12-10 20:57:49 -0500

    hide opening meeting notes

--- js/navbar.js
+++ js/navbar.js
@@ -22,9 +22,9 @@ $(document).ready(function() {
             <li class="nav-item">
               <a class="nav-link" href="tournament_rules.html">Rules</a>
             </li>
-            <li>
+<!--            <li>
               <a class="nav-link" href="meeting_notes.html">Opening Meeting Notes</a>
-            </li>
+            </li> -->
             <li class="nav-item">
               <a class="nav-link" href="tournament_documents.html">Super Tournament Documents</a>
             </li>
