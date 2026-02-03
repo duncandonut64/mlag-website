d6e8c97645ec302739e5493b0928eb5694b87255
Duncan Chen <dpatchen@umich.edu>
2026-02-02 20:32:43 -0500

    Added new social media to footer and to news

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     <hr>
         <footer class="px-4 py-2">
             <p>Copyright &copy; 2023 - 2025 David Chen and Yuxuan Chen</p>
-            <a class="mlag-social-media" href="https://www.facebook.com/AcademicgamesMIs"><i class="fa-brands fa-square-facebook"></i></a>
+            <a class="mlag-social-media" href="https://www.facebook.com/AcademicgamesMI"><i class="fa-brands fa-square-facebook"></i></a>
+            <a class="mlag-social-media" href="https://www.instagram.com/michiganleagueofacademicgames/"><i class="fa-brands fa-square-instagram"></i></a>
+            <a class="mlag-social-media" href="https://www.tiktok.com/@mlagontiktok?is_from_webapp=1&sender_device=pc"><i class="fa-brands fa-tiktok"></i></a>
         </footer>`;
 });
